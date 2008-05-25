81ff54b1f160289818ad61f39a536615a6511a60
Atul Varma <varmaa@gmail.com>
2008-05-25 18:17:54 +0000

    index.js now creates links that point to parchment.min.html if the current page is index.min.html.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function processData(stories) {
+  var parchment = "parchment.html";
+
+  if (window.location.pathname.indexOf(".min") != -1)
+    parchment = "parchment.min.html";
+
   for (var i = 0; i < stories.length; i++) {
     $("#content").append(
-      '<div class="story"><a href="parchment.html?story=' +
+      '<div class="story"><a href="' + parchment + '?story=' +
       stories[i].path +
       '">' + stories[i].desc.entityify() + '</a></div>'
     );
