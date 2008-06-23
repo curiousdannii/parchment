412c298622cbd16ff8b5d3773652c4cd2a3649e3
Atul Varma <varmaa@gmail.com>
2008-06-23 06:04:25 +0000

    Added test_storew, which tests to ensure that the particular bug behind testparchment4.z5 in issue 29, is not present.  It currently fails, though, because a fix hasn't yet been committed (that will come soon).

---
+++ tests/test_storew.js
@@ -0,0 +1,40 @@
+load("remedial.js");
+load("error-handling.js");
+load("gnusto-engine.js");
+
+function step() {
+    var retval = 0;
+
+    engine.run();
+    var effect = engine.effect(0);
+    var text = engine.consoleText();
+
+    effect = '"' + effect + '"';
+
+    switch (effect) {
+    case GNUSTO_EFFECT_QUIT:
+        retval = 1;
+        break;
+    default:
+        break;
+    };
+
+    if (text)
+        print(text);
+
+    return retval;
+}
+
+var engine = new GnustoEngine();
+
+var f = new java.io.File("stories/test_storew.z5");
+var fis = new java.io.FileInputStream(f);
+var dis = new java.io.DataInputStream(fis);
+
+var bytes = new Array(f.length());
+for (var i = 0; i < bytes.length; i++)
+  bytes[i] = dis.readUnsignedByte();
+
+engine.loadStory(bytes);
+
+while (step() == 0) {}
