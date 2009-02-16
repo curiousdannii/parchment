4876a3915b3f22bd57a736d77de28b9123d1137c
Fredrik Ramsberg <fredrik.ramsberg@gmail.com>
2009-02-16 09:30:57 +0000

    Fixes issue 72 - the statusline scrolls off the page in MSIE 6.

--- web-zui.js
+++ web-zui.js
@@ -927,3 +927,21 @@ $(document).ready(function() {
     jQuery.getScript(ZCODE_APPSPOT_URL + "?url=" + escape(story) +
                      "&jsonp=processZcodeAppspotResponse");
 });
+
+var topwin_element;
+var topwin_dist = '0';
+
+// Make the statusline always move to the top of the screen in MSIE < 7
+$(document).ready(function() {
+    topwin_element = document.getElementById('top-window');
+    topwin_dist = '0';
+    var ieMatch = navigator.appVersion.match(/MSIE (\d+)\./);
+    if(ieMatch && +ieMatch[1]<7) {
+        topwin_element.style.position = 'absolute';
+        var move_element=function() {
+            topwin_element.style.top = 1 * (document.documentElement.scrollTop + 1 * topwin_dist) + 'px';
+        };
+        window.onscroll = move_element;
+        window.onresize = move_element;
+    }
+});
