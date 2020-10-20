a97a1fe566eb1acbf08d655d338d9a720e6557bf
Juhana Leinonen <juhana@users.noreply.github.com>
2020-10-21 01:18:27 +0300

    More accurate file format detection (#73)

--- src/inform7/index.js
+++ src/inform7/index.js
@@ -42,11 +42,11 @@ function launch()
     // Discriminate
     const storyfilepath = options.default_story[0]
     let format
-    if (/zblorb|z3|z4|z5|z8/.test(storyfilepath))
+    if (/\.(zblorb|z3|z4|z5|z8)(\.js)?$/.test(storyfilepath))
     {
         format = 'zcode'
     }
-    else if (/gblorb|ulx/.test(storyfilepath))
+    else if (/\.(gblorb|ulx)(\.js)?$/.test(storyfilepath))
     {
         format = 'glulx'
     }
