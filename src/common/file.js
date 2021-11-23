c733bc493e7fe406cd4b53d5ac91b372d70dc4a9
Dannii Willis <curiousdannii@gmail.com>
2021-11-24 09:47:48 +1000

    Support JSified stories

--- src/common/file.js
+++ src/common/file.js
@@ -3,7 +3,7 @@
 File loader
 ===========
 
-Copyright (c) 2020 Dannii Willis
+Copyright (c) 2021 Dannii Willis
 MIT licenced
 https://github.com/curiousdannii/parchment
 
@@ -11,7 +11,7 @@ https://github.com/curiousdannii/parchment
 
 async function fetch_storyfile(options, url)
 {
-    const response = await fetch(url)
+    let response = await fetch(url)
     .catch(e => {
         return fetch(`${options.proxy_url}?url=${url}`)
     })
@@ -19,6 +19,18 @@ async function fetch_storyfile(options, url)
     if (!response.ok)
     {
         throw new Error(`Could not fetch storyfile, got ${response.status}`)
+    }
+
+    // Handle JSified stories
+    if (url.endsWith('.js')) {
+        const text = await response.text()
+        const matched = /processBase64Zcode\(['"]([a-zA-Z0-9+\/=]+)['"]\)/.exec(text)
+        if (!matched) {
+            throw new Error('Abnormal JSified story')
+        }
+
+        // Parse the base64 using a trick from https://stackoverflow.com/a/54123275/2854284
+        response = await fetch(`data:application/octet-binary;base64,${matched[1]}`)
     }
 
     const buffer = await response.arrayBuffer()
