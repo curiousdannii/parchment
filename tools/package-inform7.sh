072d4527173aa9635b51840c63a297aeb4baf6db
Dannii Willis <curiousdannii@gmail.com>
2021-11-30 12:04:31 +1000

    Only update and deploy parchment-for-inform7.zip if the contents have changed

--- tools/package-inform7.sh
+++ tools/package-inform7.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd "$(dirname "$0")/.."
+
 npm run gulp inform7
 
 sed "s/DATE/$(date '+%Y.%m')/" src/inform7/manifest.txt > 'dist/inform7/Parchment/(manifest).txt'
@@ -7,4 +9,21 @@ cp node_modules/jquery/dist/jquery.min.js dist/inform7/Parchment
 cp src/upstream/quixe/media/resourcemap.js dist/inform7/Parchment
 cp src/upstream/glkote/waiting.gif dist/inform7/Parchment
 
-cd dist/inform7/ && zip -r parchment-for-inform7.zip Parchment
+cd dist/inform7
+
+# Download the zip so we can check file hashes
+if [ ! -f parchment-for-inform7.zip ]; then
+    echo "Downloading old parchment-for-inform7.zip"
+    curl -s -L https://github.com/curiousdannii/parchment/raw/gh-pages/dist/inform7/parchment-for-inform7.zip -o parchment-for-inform7.zip
+fi
+
+# Unzip the old zip
+unzip -joq parchment-for-inform7.zip -d Parchment-old
+
+diff Parchment Parchment-old >/dev/null
+if [ $? -ne 0 ]; then
+    echo "Files changed; updating parchment-for-inform7.zip"
+    zip -r parchment-for-inform7.zip Parchment
+else
+    echo "Files are unchanged"
+fi
