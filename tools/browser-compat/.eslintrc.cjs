ad9610a6194dd13304acfbc3813f7a36a3e8be5d
Dannii Willis <curiousdannii@gmail.com>
2022-01-28 17:31:43 +1000

    AsyncGlk update for iOS Support high pixel density canvases

--- tools/browser-compat/.eslintrc.cjs
+++ tools/browser-compat/.eslintrc.cjs
@@ -7,7 +7,10 @@ module.exports = {
     root: true,
     settings: {
         polyfills: [
-            "WebAssembly.instantiateStreaming",
+            // The Eslint compat plugin doesn't support minimised feature detection: https://github.com/amilajack/eslint-plugin-compat/issues/508
+            // Adding these here doesn't kind make the plugin pointless, but at least we'll still see if anything new starts being used
+            'ResizeObserver',
+            'WebAssembly.instantiateStreaming',
         ]
     }
 }
