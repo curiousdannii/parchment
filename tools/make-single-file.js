15a646654c6d007b7533971e4b662f24af8f1299
Dannii Willis <curiousdannii@gmail.com>
2022-04-10 16:11:22 +1000

    Improve make-single-file.js: don't rezip if unchanged, date mode

--- tools/make-single-file.js
+++ tools/make-single-file.js
@@ -11,12 +11,15 @@ https://github.com/curiousdannii/parchment
 */
 
 import child_process from 'child_process'
+import crypto from 'crypto'
 import fs from 'fs/promises'
 import path from 'path'
 import {fileURLToPath} from 'url'
 import util from 'util'
 
 const execFile = util.promisify(child_process.execFile)
+
+const datemode = process.argv.includes('--date')
 
 const rootpath = path.join(path.dirname(fileURLToPath(import.meta.url)), '..')
 const webpath = path.join(rootpath, 'dist/web')
@@ -35,6 +38,9 @@ const files = await Promise.all(filenames.map(async filename => {
         return
     }
     let data = await fs.readFile(path.join(webpath, filename), {encoding: filename.endsWith('.wasm') ? null : 'utf8'})
+    if (!filename.endsWith('.wasm')) {
+        data = data.trim()
+    }
     if (filename === 'ie.js') {
         return `<script nomodule>${data}</script>`
     }
@@ -73,17 +79,51 @@ indexhtml = indexhtml
     .replace(/<img src="dist\/web\/waiting.gif"/, `<img src="data:image/gif;base64,${await file_to_base64(path.join(webpath, 'waiting.gif'))}"`)
     .replace(/^\s+$/gm, '')
 
+// Add a date if requested
+if (datemode) {
+    const today = new Date()
+    const title = `Parchment ${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}`
+    indexhtml = indexhtml
+        .replace(/<title.+?\/title>/, `<title>${title}</title>`)
+        .replace(/<\/noscript>/, `</noscript>\n<p id="footer-date">${title}</p>`)
+}
+
 const parts = indexhtml.split(/\s*<\/head>/)
 indexhtml = `${parts[0]}
 <script>parchment_options = {single_file: 1}</script>
 ${files.filter(file => file).join('\n')}
 </head>${parts[1]}`
 
-// Write out
 const outdir = path.join(webpath, '../single-file')
 await fs.mkdir(outdir, {recursive: true})
 const outpath = path.join(outdir, 'parchment.html')
-await fs.writeFile(outpath, indexhtml)
 
-// Zip it
-await execFile('zip', ['-j', '-r', path.join(outdir, 'parchment-single-file.zip'), outpath])
+// Check if the file has changed
+const oldhash = crypto.createHash('sha1').setEncoding('hex')
+try {
+    oldhash.write(await fs.readFile(outpath))
+}
+catch (_) {}
+oldhash.end()
+const newhash = crypto.createHash('sha1').setEncoding('hex')
+newhash.write(indexhtml)
+newhash.end()
+
+if (oldhash.read() !== newhash.read()) {
+    // Write it out
+    console.log('Creating dist/single-file/parchment.html')
+    await fs.writeFile(outpath, indexhtml)
+
+    // Zip it
+    let zipname = `parchment-single-file.zip`
+    if (datemode) {
+        const today = new Date()
+        zipname = `parchment-single-file-${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}.zip`
+    }
+    console.log(`Zipping dist/single-file/${zipname}`)
+    const result = await execFile('zip', ['-j', '-r', path.join(outdir, zipname), outpath])
+    console.log(result.stdout.trim())
+}
+else {
+    console.log('Single file parchment.html is unchanged')
+}
