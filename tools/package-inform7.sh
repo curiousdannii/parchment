37da87646f576f37ca2ea67debe8bab194c3583f
Dannii Willis <curiousdannii@gmail.com>
2022-04-12 09:00:22 +1000

    Update README [ci skip]

--- tools/package-inform7.sh
+++ tools/package-inform7.sh
@@ -5,9 +5,6 @@ cd "$(dirname "$0")/.."
 ./build.js inform7
 
 sed "s/DATE/$(date '+%Y.%-m')/" src/inform7/manifest.txt > 'dist/inform7/Parchment/(manifest).txt'
-cp node_modules/jquery/dist/jquery.min.js dist/inform7/Parchment
-cp src/upstream/quixe/media/resourcemap.js dist/inform7/Parchment
-cp src/upstream/glkote/waiting.gif dist/inform7/Parchment
 
 cd dist/inform7
 
