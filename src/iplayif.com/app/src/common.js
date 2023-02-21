66aef4776b8d259e382aa08030c3042a8bbe10ac
Dannii Willis <curiousdannii@gmail.com>
2023-02-21 17:45:23 +1000

    Add support for Adrift 4 files

--- src/iplayif.com/app/src/common.js
+++ src/iplayif.com/app/src/common.js
@@ -3,10 +3,10 @@
 Common things
 =============
 
-Copyright (c) 2022 Dannii Willis
+Copyright (c) 2023 Dannii Willis
 MIT licenced
 https://github.com/curiousdannii/iplayif.com
 
 */
 
-export const SUPPORTED_TYPES = /\.(blb|blorb|gam|gblorb|glb|hex|t3|ulx|zblorb|zlb|z[3458])$/i
+export const SUPPORTED_TYPES = /\.(blb|blorb|gam|gblorb|glb|hex|taf|t3|ulx|zblorb|zlb|z[3458])$/i
